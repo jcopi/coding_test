2966adb5869a7538efe1c4006294956f8fd1bf09
Joel Copi <joel@copi.dev>
2021-08-08 18:04:23 -0400

    finalizing api server

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/jcopi/coding_test
+
+go 1.16
+
+require (
+	github.com/gin-gonic/gin v1.7.3 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	go.etcd.io/etcd/client/v3 v3.5.0 // indirect
+	go.uber.org/zap v1.18.1 // indirect
+)
--- main.go
+++ main.go
@@ -7,18 +7,17 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
-	"go.etcd.io/etcd/clientv3"
+	clientv3 "go.etcd.io/etcd/client/v3"
 	"go.uber.org/zap"
 )
 
 const (
 	loggerKey = "logger"
 	etcdKey   = "etcd"
-	itemKey   = "key"
 	reqIDKey  = "requestid"
 )
 
-type value struct {
+type itemValue struct {
 	Value string `json:"value"`
 }
 
@@ -32,10 +31,10 @@ func mustGetLogger(c *gin.Context) *zap.Logger {
 	logger := c.MustGet(loggerKey).(*zap.Logger)
 	reqID, ok := c.Get(reqIDKey)
 	if !ok {
-		reqID = uuid.NewV4().String()
+		reqID = uuid.New().String()
 	}
 	setInContext(reqIDKey, reqID)
-	logger = logger.With(zap.String("req_id", reqID))
+	logger = logger.With(zap.String("req_id", reqID.(string)))
 	return logger
 }
 
@@ -45,23 +44,19 @@ func mustGetEtcd(c *gin.Context) *clientv3.Client {
 	return client
 }
 
-func mustGetKey(c *gin.Context) string {
-	return c.MustGet(itemKey).(string)
-}
-
 func GetItem(c *gin.Context) {
 	logger := mustGetLogger(c).With(zap.String("method", "GetItem"))
 	client := mustGetEtcd(c)
 
-	key := mustGetKey(c)
+	key := c.Param("key")
 	if len(key) < 1 {
 		logger.Error("invalid key")
 		c.AbortWithStatus(http.StatusBadRequest)
 		return
 	}
 
-	var val value
-	etcdKey := "/item/" + key
+	var val itemValue
+	etcdKey := "/items/" + key
 
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
@@ -88,27 +83,52 @@ func SetItem(c *gin.Context) {
 	logger := mustGetLogger(c).With(zap.String("method", "SetItem"))
 	client := mustGetEtcd(c)
 
-	key := mustGetKey(c)
+	key := c.Param("key")
 	if len(key) < 1 {
 		logger.Error("invalid key")
 		c.AbortWithStatus(http.StatusBadRequest)
 		return
 	}
 
-	var val value
+	var val itemValue
 	if err := c.BindJSON(&val); err != nil {
 		logger.Error("error unmarshalling request", zap.Error(err))
 		c.AbortWithStatus(http.StatusBadRequest)
 		return
 	}
 
-	etcdKey := "/item/" + key
+	etcdKey := "/items/" + key
 
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 	_, err := client.Put(ctx, etcdKey, val.Value)
 	if err != nil {
-		logger.Error("error on get", zap.String("key", etcdKey), zap.Error(err))
+		logger.Error("error on set", zap.String("key", etcdKey), zap.Error(err))
+		c.AbortWithStatus(http.StatusInternalServerError)
+		return
+	}
+
+	c.Status(http.StatusOK)
+}
+
+func DeleteItem(c *gin.Context) {
+	logger := mustGetLogger(c).With(zap.String("method", "DeleteItem"))
+	client := mustGetEtcd(c)
+
+	key := c.Param("key")
+	if len(key) < 1 {
+		logger.Error("invalid key")
+		c.AbortWithStatus(http.StatusBadRequest)
+		return
+	}
+
+	etcdKey := "/items/" + key
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	_, err := client.Delete(ctx, etcdKey)
+	if err != nil {
+		logger.Error("error on delete", zap.String("key", etcdKey), zap.Error(err))
 		c.AbortWithStatus(http.StatusInternalServerError)
 		return
 	}
@@ -126,8 +146,9 @@ func NewServer(logger *zap.Logger, etcdClient *clientv3.Client) *gin.Engine {
 	)
 
 	api := s.Group("/api")
-	api.GET("/:key", GetItem)
-	api.POST("/:key", SetItem)
+	api.GET("/items/*key", GetItem)
+	api.POST("/items/*key", SetItem)
+	api.DELETE("/items/*key", DeleteItem)
 
 	return s
 }
@@ -138,8 +159,7 @@ func main() {
 		panic(err)
 	}
 	client, err := clientv3.New(clientv3.Config{
-		Endpoints:   []string{"http://127.0.0.1:2379"},
-		DialTimeout: 5 * time.Second,
+		Endpoints: []string{"http://0.0.0.0:2379"},
 	})
 	if err != nil {
 		panic(err)
